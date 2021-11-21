fa5e2156c5ba544ea4abe4c33067b69cd5fd51ed
matv <78199275+matv1346@users.noreply.github.com>
2021-11-21 14:05:32 +0300

    Add files via upload

---
+++ Project4.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+int o;
+char* a = new char[o];
+
+void input()
+{
+    cout << "Введите количество оценок:";
+    cin >> o;
+    cout << "Введите все оценки без пробелов:";
+    for (int i = 0; i < o; i++)
+    {
+        cin >> a[i];
+    }
+    
+}
+void math()
+{
+    int five, fore, three, two;
+    five = fore = three = two = 0;
+    for (int i = 0; i < o; i++)
+    {
+        switch (a[i]) {
+        case '5':
+            five++;
+            break;
+        case '4':
+            fore++;
+            break;
+        case'3':
+            three++;
+            break;
+        case'2':
+            two++;
+            break;
+        default:
+            cout << "Ошибка: число не является областью определения [2;5]";
+            break;
+        }
+    }
+
+    five *= 100;
+    fore *= 50;
+    three *= 50;
+    two *= 100;
+    int sum =  fore + five - two - three;
+    cout << sum;
+}
+int main()
+{
+    input();
+    math();
+    delete[]a;
+    a = nullptr;
+    return 0;
+}
+
